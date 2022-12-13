48b34b57c5fd5dc161a5cd19b8b58311797bf9ff
David Chatel <dchatel@live.fr>
2022-12-13 18:48:05 +0100

    Revert "removed useless viewmodels"
    This reverts commit 8065311dc1f0674debfd697f2d8b07c4b91f0998.

---
+++ CouchPotato/Views/VideoExplorer/VideoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CouchPotato.DbModel;
+
+using PostSharp.Patterns.Model;
+
+namespace CouchPotato.Views.VideoExplorer;
+
+public class VideoViewModel
+{
+    private Video video;
+
+    public VideoViewModel(Video video)
+    {
+        this.video = video;
+    }
+
+    public string Title => video.Title;
+}
